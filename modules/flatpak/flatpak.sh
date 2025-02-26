a2eb08bf617fdaa2bbafceab7e422ebf12292b83
tywil04 <github.mandatory@pretend.email>
2025-02-26 10:01:32 +0000

    changes

--- modules/flatpak/flatpak.sh
+++ modules/flatpak/flatpak.sh
@@ -10,26 +10,10 @@ echo "Install List: ${INSTALL[@]}"
 echo "Copying files"
 cp -r "$MODULE_DIRECTORY/flatpak/files/"* "/"
 
-echo "Adding flathub as flatpak remote"
+echo "Adding flathub remote"
 flatpak remote-add --installation="tylers-os" --if-not-exists "flathub" "https://dl.flathub.org/repo/flathub.flatpakrepo"
 
-echo "Installing packages from install list"
+echo "Installing packages"
 flatpak install --installation="tylers-os" --assumeyes --noninteractive "${INSTALL[@]}"
-
-# echo "Modifying flathub remote"
-# flatpak remote-modify --installation="tylers-os" --collection-id="org.flathub.Stable" "flathub"
-
-# echo "Creating offline flatpak repo"
-# flatpak create-usb "/usr/share/tylers-os/flatpak" "${INSTALL[@]}"
-# mv "/usr/share/tylers-os/flatpak/.ostree/repo" "/usr/share/tylers-os/flatpak/offline-repo"
-# rm -r "/usr/share/tylers-os/flatpak/.ostree"
-
-# echo "Saving install list for setup"
-# for APP in ${INSTALL[@]}; do
-#     echo $APP >> "/usr/share/tylers-os/flatpak/install-list"
-# done
-
-# echo "Enabling setup service"
-# systemctl enable -f tylers-os-flatpak-setup.service
 
 echo "Done"
