a284502f242afa31119efd46deb68da65a8f9501
Tyler <github.mandatory@pretend.email>
2025-02-26 06:46:45 +0000

    Update flatpak.sh

--- modules/flatpak/flatpak.sh
+++ modules/flatpak/flatpak.sh
@@ -19,18 +19,13 @@ echo "Adding flathub as flatpak remote"
 flatpak remote-add --if-not-exists "flathub" "https://dl.flathub.org/repo/flathub.flatpakrepo"
 
 echo "Installing packages from install list"
-flatpak install --assumeyes "${INSTALL[@]}"
+flatpak install --assumeyes --noninteractive "${INSTALL[@]}"
 
 echo "Modifying flathub remote"
 flatpak remote-modify --collection-id="org.flathub.Stable" "flathub"
 
 echo "Creating offline flatpak repo"
 flatpak create-usb "/usr/share/tylers-os/flatpak/offline-repo" "${INSTALL[@]}"
-
-echo "Saving install list for post rebase setup"
-for APP in ${INSTALL[@]}; do
-    echo $APP >> "/usr/share/tylers-os/flatpak/install-list"
-done
 
 echo "Saving install list for post rebase setup"
 for APP in ${INSTALL[@]}; do
