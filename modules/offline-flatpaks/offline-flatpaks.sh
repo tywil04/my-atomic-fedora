f30d5a63d4b2ce88eae7305ce4baf967a293d452
Tyler <github.mandatory@pretend.email>
2025-02-26 04:30:09 +0000

    Update offline-flatpaks.sh

--- modules/offline-flatpaks/offline-flatpaks.sh
+++ modules/offline-flatpaks/offline-flatpaks.sh
@@ -3,6 +3,8 @@
 # Tell build process to exit if there are any errors.
 set -euo pipefail
 
+OUTPUT_DIR="/usr/share/tylers-os/offline-flatpaks"
+
 get_json_array INSTALL "try .install[]" "$1"
 REPO_URL=$(echo "$1" | jq -r 'try .["repo-url"]')
 REPO_NAME=$(echo "$1" | jq -r 'try .["repo-name"]')
@@ -20,7 +22,7 @@ echo "Collection ID: $COLLECTION_ID"
 echo "Install List: ${INSTALL[@]}"
 
 echo "Making offline-flatpaks directory"
-mkdir -p /usr/share/tylers-os/offline-flatpaks
+mkdir -p "$OUTPUT_DIR"
 
 echo "Adding flathub as flatpak remote"
 flatpak remote-add --if-not-exists "$REPO_NAME" "$REPO_URL"
@@ -32,4 +34,11 @@ echo "Modifying flathub remote"
 flatpak remote-modify --collection-id="$COLLECTION_ID" "$REPO_NAME"
 
 echo "Creating offline flatpak repo"
-flatpak create-usb /usr/share/tylers-os/offline-flatpaks "${INSTALL[@]}"
+flatpak create-usb "$OUTPUT_DIR" "${INSTALL[@]}"
+
+echo "Saving install list for post rebase setup"
+for $APP in $INSTALL; do
+    echo $APP >> "$OUTPUT_DIR/install-list"
+done
+
+echo "Done"
