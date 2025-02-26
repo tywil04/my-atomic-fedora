e7aadc687716a62f266bd8c5b24df31b75adc3da
Tyler <github.mandatory@pretend.email>
2025-02-26 02:47:50 +0000

    Update offline-flatpaks.sh

--- modules/offline-flatpaks/offline-flatpaks.sh
+++ modules/offline-flatpaks/offline-flatpaks.sh
@@ -14,10 +14,15 @@ if [[ $REPO_URL == "null" || $REPO_NAME == "null" || $COLLECTION_ID == "null" ]]
     COLLECTION_ID="org.flathub.Stable"
 fi
 
+echo "Offline Flatpaks: Repo URL = $REPO_URL"
+echo "Offline Flatpaks: Repo Name = $REPO_NAME"
+echo "Offline Flatpaks: Collection Id = $COLLECTION_ID"
+echo "Offline Flatpaks: Install List = $INSTALL"
+
 mkdir -p /usr/share/tylers-os/offline-flatpaks
 
-flatpak remote-add --if-not-exists $REPO_NAME $REPO_URL
+flatpak remote-add --if-not-exists "$REPO_NAME" "$REPO_URL"
 
-flatpak remote-modify --collection-id=$COLLECTION_ID $REPO_NAME
+flatpak remote-modify --collection-id="$COLLECTION_ID" "$REPO_NAME"
 
 flatpak create-usb /usr/share/tylers-os/offline-flatpaks "${INSTALL[@]}"
